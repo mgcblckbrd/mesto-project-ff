4dd93530327276459fdc61e91d35ced1191a07bd
Anton Grigorev <seosohard@ya.ru>
2024-04-30 21:55:00 +0300

    добавил закрытие попапов по клику вне и эскейпу

--- src/components/card.js
+++ src/components/card.js
@@ -17,4 +17,6 @@ function removeCard(evt) {
   card.remove();
 }
 
+
+
 export { createCard, removeCard };
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,33 +1,45 @@
-// это работа кнопки закрытия
-function closedPopup(item) {
-    item.classList.remove("popup_is-opened");
-  }
-  
-  const closedPopupsButtons = document.querySelectorAll(".popup__close");
-  
-  closedPopupsButtons.forEach((closeButton) => {
-    const popup = closeButton.closest(".popup");
-    closeButton.addEventListener("click", () => {
-      closedPopup(popup);
-    });
-  });
-  const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfilePopup = document.querySelector(".popup_type_edit");
-
 const addProfileButton = document.querySelector(".profile__add-button");
 const addCardPopup = document.querySelector(".popup_type_new-card");
-// колбэк открытия полапов
-function openPopup(evt) {
-    evt.classList.add("popup_is-opened");
-  }
-  
-  // слушатели открытия попапов
-  editProfileButton.addEventListener("click", () => {
-    openPopup(editProfilePopup);
-  });
-  
-  addProfileButton.addEventListener("click", () => {
-    openPopup(addCardPopup);
-  });
+const closedPopupsButtons = document.querySelectorAll(".popup__close");
 
-  export {closedPopup, openPopup};
+function openPopup(evt) {
+
+  evt.classList.add("popup_is-animated");
+  evt.classList.add("popup_is-opened");
+  document.addEventListener("keydown", function (evt) {
+    if (evt.key === "Escape") {
+      const popup = document.querySelector(".popup_is-opened");
+      if (popup) {
+        closedPopup(popup);
+      }
+    }
+  });
+}
+
+editProfileButton.addEventListener("click", () => {
+  openPopup(editProfilePopup);
+});
+
+addProfileButton.addEventListener("click", () => {
+  openPopup(addCardPopup);
+});
+
+function closedPopup(item) {
+  item.classList.remove("popup_is-opened");
+}
+
+closedPopupsButtons.forEach((closeButton) => {
+  const popup = closeButton.closest(".popup");
+  closeButton.addEventListener("click", () => {
+    closedPopup(popup);
+  });
+  popup.addEventListener("mousedown", (evt) => {
+    if (evt.target.classList.contains("popup")) {
+      closedPopup(evt.target);
+    }
+  });
+});
+
+export { closedPopup, openPopup };
